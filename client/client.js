8934d90f883c7a404710ff96a79820739c420dcd
iKittyCode <gator.syon@gmail.com>
2020-11-18 17:59:30 -0500

    Changing the files to make it not have a canvas Syoon Hope  you like this

--- client/client.js
+++ client/client.js
@@ -1,6 +1,7 @@
 var name = prompt("What is your name")
 var input = prompt("What is your age")
 function setup() {
+    noCanvas()
 var button = createButton('Click to resubmit age')
 button.mousePressed(function() {
     var name = prompt("What is your name")
