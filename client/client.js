00c17597ace3320666d84e8999bb92b11c3c0cca
iKittyCode <gator.syon@gmail.com>
2020-11-19 12:05:45 -0500

    Dear everybody I have worked on my server so hard So please thank you

--- client/client.js
+++ client/client.js
@@ -1,24 +1,21 @@
 var name = prompt("What is your name")
-var input = prompt("What is your age")
+var input = prompt("what your age")
+
 function setup() {
     noCanvas()
-var button = createButton('Click to resubmit age')
-button.mousePressed(function() {
-    var name = prompt("What is your name")
-    var input = prompt("What is your age")
-    fetchMe()
-})
+
 }
-async function fetchMe() {
-var api = fetch('/api', {
-method:'POST',
-headers:{
-    'Content-Type': 'application/json'
-},
-body:JSON.stringify({
-name:name,
-age:input
-})
-})
+
+function fetchMe() {
+    var api = fetch('/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            age: input
+        })
+    })
 }
 fetchMe()
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 var express = require("express")
 var app = express()
-app.listen(8080, function() {
-console.log("😹 Running at http://localhost:8080")
+
+
+app.listen(8080, function () {
+    console.log("😹Running at http://localhost:8080")
 })
 
 app.use(express.static('client'))
 app.use(express.json())
-app.post('/api', function(req, res) {
-if (req.body.age == '' || req.body.name == '') {
-console.log("Sorry this is and empty request")
-} else {
-console.log("name: " + req.body.name)
-console.log("age: " + req.body.age)
-}
+app.post('/api', function (req, res) {
+    console.log(req.body)
+    res.send('Thanks for sending')
 })
