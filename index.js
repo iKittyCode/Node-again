2f70cd10de24dde99d47ea50a3d83137ea67558f
iKittyCode <gator.syon@gmail.com>
2020-11-20 16:26:49 -0500

    I love databases So i added one

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express")
 var app = express()
-
-
+var Datastore  = require('nedb')
+var db = new Datastore({ filename:"MyClients.db", autoload:true})
 app.listen(8080, function () {
     console.log("😹Running at http://localhost:8080")
 })
@@ -10,12 +10,8 @@ app.use(express.static('client'))
 app.use(express.json())
 app.post('/api', function (req, res) {
     console.log(req.body)
+    db.insert(req.body, function(err, doc) {
+        console.log("Data sent");
+    });
     
-
-    res.send('Thanks for sending')
-    
-})
-app.get('/api', function(request, response) {
-response.send("<h1>Thanks for sending data</h1> ")
-
 })
